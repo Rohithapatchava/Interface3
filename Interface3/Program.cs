fdcceaee1326ad40a051b29b9059bc0bcf20e01a
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-19 17:54:05 +0530

    Initial commit

---
+++ Interface3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface3
+{
+    interface Iairborne
+    {
+        void fly();
+    }
+    class helicopter : Iairborne
+    {
+        public void fly()
+        {
+            Console.WriteLine("hello helicopter");
+        }
+    }
+    class Aeroplane : Iairborne
+    {
+        public void fly()
+        {
+            Console.WriteLine("hello aeroplane");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            helicopter h = new helicopter();
+            Aeroplane a = new Aeroplane();
+            h.fly();
+            a.fly();
+        }
+    }
+}
